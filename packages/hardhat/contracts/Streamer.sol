f9acd7454516064924d73e9fb45568b504f5a70a
Syed Ghufran Hassan <syedghufranhassan@gmail.com>
2023-12-26 21:37:07 +0500

    updated

--- packages/hardhat/contracts/Streamer.sol
+++ packages/hardhat/contracts/Streamer.sol
@@ -1,92 +1,159 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity 0.8.4;
+
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "hardhat/console.sol";
 
+
+error Streamer__User_already_running_a_channel();
+error Streamer__Less_than_required_amount();
+error Streamer__Signer_is_not_running_the_channel();
+error Streamer__Transfer_failed();
+error Streamer__User_channel_is_not_open();
+error Streamer__User_channel_is_not_closed();
+error Streamer__User_time_is_not_before_current_time();
+
+
+/**
+ * @title State Channel Application.
+ * @author ABossOfMyself
+ * @notice A minimal state channel application.
+ * @dev Created a minimal state channel application, where users seeking a service lock collateral on-chain with a single transaction, interact with their service provider entirely off-chain, and finalize the interaction with a second on-chain transaction.
+ */
+
+
 contract Streamer is Ownable {
-  event Opened(address, uint256);
-  event Challenged(address);
-  event Withdrawn(address, uint256);
-  event Closed(address);
 
-  mapping(address => uint256) balances;
-  mapping(address => uint256) canCloseAt;
+    /* ========== EVENTS ========== */
 
-  function fundChannel() public payable {
-    /*
-      Checkpoint 2: fund a channel
 
-      Complete this function so that it:
-      - reverts if msg.sender already has a running channel (ie, if balances[msg.sender] != 0)
-      - updates the balances mapping with the eth received in the function call
-      - emits an Opened event
-    */
-  }
+    event Opened(address user, uint256 amount);
 
-  function timeLeft(address channel) public view returns (uint256) {
-    if (canCloseAt[channel] == 0 || canCloseAt[channel] < block.timestamp) {
-      return 0;
+    event Challenged(address user);
+
+    event Withdrawn(address user, uint256 amount);
+
+    event Closed(address user);
+
+
+    /* ========== MAPPINGS ========== */
+
+
+    mapping(address => uint256) balances;
+
+    mapping(address => uint256) canCloseAt;
+
+
+    /* ========== MUTATIVE FUNCTIONS ========== */
+
+
+    function fundChannel() public payable {
+        
+        if(balances[msg.sender] != 0) revert Streamer__User_already_running_a_channel();
+
+        if(msg.value < 0.5 ether) revert Streamer__Less_than_required_amount();
+
+        balances[msg.sender] = msg.value;
+
+        emit Opened(msg.sender, msg.value);
     }
 
-    return canCloseAt[channel] - block.timestamp;
-  }
 
-  function withdrawEarnings(Voucher calldata voucher) public {
-    // like the off-chain code, signatures are applied to the hash of the data
-    // instead of the raw data itself
-    bytes32 hashed = keccak256(abi.encode(voucher.updatedBalance));
 
-    // The prefix string here is part of a convention used in ethereum for signing
-    // and verification of off-chain messages. The trailing 32 refers to the 32 byte
-    // length of the attached hash message.
-    //
-    // There are seemingly extra steps here compared to what was done in the off-chain
-    // `reimburseService` and `processVoucher`. Note that those ethers signing and verification
-    // functions do the same under the hood.
-    //
-    // see https://blog.ricmoo.com/verifying-messages-in-solidity-50a94f82b2ca
-    bytes memory prefixed = abi.encodePacked("\x19Ethereum Signed Message:\n32", hashed);
-    bytes32 prefixedHashed = keccak256(prefixed);
+    function timeLeft(address channel) public view returns(uint256) {
 
-    /*
-      Checkpoint 4: Recover earnings
+        if(canCloseAt[channel] == 0) revert Streamer__User_channel_is_not_closed();
 
-      The service provider would like to cash out their hard earned ether.
-          - use ecrecover on prefixedHashed and the supplied signature
-          - require that the recovered signer has a running channel with balances[signer] > v.updatedBalance
-          - calculate the payment when reducing balances[signer] to v.updatedBalance
-          - adjust the channel balance, and pay the Guru(Contract owner). Get the owner address with the `owner()` function.
-          - emit the Withdrawn event
-    */
-  }
+        return canCloseAt[channel] - block.timestamp;
+    }
 
-  /*
-    Checkpoint 5a: Challenge the channel
 
-    Create a public challengeChannel() function that:
-    - checks that msg.sender has an open channel
-    - updates canCloseAt[msg.sender] to some future time
-    - emits a Challenged event
-  */
 
-  /*
-    Checkpoint 5b: Close the channel
+    function withdrawEarnings(Voucher calldata voucher) public onlyOwner {
 
-    Create a public defundChannel() function that:
-    - checks that msg.sender has a closing channel
-    - checks that the current time is later than the closing time
-    - sends the channel's remaining funds to msg.sender, and sets the balance to 0
-    - emits the Closed event
-  */
+        // like the off-chain code, signatures are applied to the hash of the data
+        // instead of the raw data itself
 
-  struct Voucher {
-    uint256 updatedBalance;
-    Signature sig;
-  }
-  struct Signature {
-    bytes32 r;
-    bytes32 s;
-    uint8 v;
-  }
+        bytes32 hashed = keccak256(abi.encode(voucher.updatedBalance));
+
+        // The prefix string here is part of a convention used in ethereum for signing
+        // and verification of off-chain messages. The trailing 32 refers to the 32 byte
+        // length of the attached hash message.
+        //
+        // There are seemingly extra steps here compared to what was done in the off-chain
+        // `reimburseService` and `processVoucher`. Note that those ethers signing and verification
+        // functions do the same under the hood.
+        //
+        // again, see https://blog.ricmoo.com/verifying-messages-in-solidity-50a94f82b2ca
+
+        bytes memory prefixed = abi.encodePacked("\x19Ethereum Signed Message:\n32", hashed);
+
+        bytes32 prefixedHashed = keccak256(prefixed);
+
+        address signer = ecrecover(prefixedHashed, voucher.sig.v, voucher.sig.r, voucher.sig.s);
+
+        if(balances[signer] <= voucher.updatedBalance) revert Streamer__Signer_is_not_running_the_channel();
+
+        uint256 payment = balances[signer] - voucher.updatedBalance;
+
+        balances[signer] -= payment;
+
+        address owner = owner();
+
+        (bool success, ) = owner.call{value: payment}("");
+
+        if(!success) revert Streamer__Transfer_failed();
+
+        emit Withdrawn(owner, payment);
+    }
+
+    
+
+    function challengeChannel() public {
+
+        if(balances[msg.sender] == 0) revert Streamer__User_channel_is_not_open();
+
+        canCloseAt[msg.sender] = block.timestamp + 30 seconds;
+
+        emit Challenged(msg.sender);
+    }
+
+    
+
+    function defundChannel() public {
+
+        if(canCloseAt[msg.sender] == 0) revert Streamer__User_channel_is_not_closed();
+
+        if(canCloseAt[msg.sender] > block.timestamp) revert Streamer__User_time_is_not_before_current_time();
+
+        (bool success, ) = msg.sender.call{value: balances[msg.sender]}("");
+
+        if(!success) revert Streamer__Transfer_failed();
+
+        balances[msg.sender] = 0;
+
+        emit Closed(msg.sender);
+    }
+
+
+
+    struct Voucher {
+
+        uint256 updatedBalance;
+
+        Signature sig;
+    }
+
+
+
+    struct Signature {
+
+        bytes32 r;
+
+        bytes32 s;
+
+        uint8 v;
+    }
 }
